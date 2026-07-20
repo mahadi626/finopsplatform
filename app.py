3c7151641d9aedc5fa676844e4e919c41488f6a1
mahadi626 <ahammadibnemahadi@gmail.com>
2026-07-20 14:05:50 +0600

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,8 @@ model = None
 if "GEMINI_API_KEY" in st.secrets:
     try:
         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-        model = genai.GenerativeModel('gemini-pro')
+       model = genai.GenerativeModel('models/gemini-pro')
+
     except Exception as e:
         st.error(f"Gemini API কানেকশনে সমস্যা হয়েছে: {str(e)}")
 else:
